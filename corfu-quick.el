86ddc94e3ec720771153ad52c5e94914d8e6932b
Luis Henriquez-Perez <luis@luishp.xyz>
2022-01-11 21:33:55 -0500

    Update corfu-quick

--- corfu-quick.el
+++ corfu-quick.el
@@ -82,7 +82,8 @@
 	  (push (reverse c-result) cands-result)))
       (apply orig (reverse cands-result)))))
 
-(defun corfu--quick-jump ())
+(defun corfu--quick-jump ()
+  "")
 
 ;; (defun corfu-quick--read ()
 ;;   "Read quick key given FIRST pressed key."
