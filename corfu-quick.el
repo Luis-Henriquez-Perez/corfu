eaaf93d6ee522951e89802ca57139ee248636232
Luis Henriquez-Perez <luis@luishp.xyz>
2022-01-12 11:14:16 -0500

    Update corfu-quick

--- corfu-quick.el
+++ corfu-quick.el
@@ -114,6 +114,7 @@
   (cl-letf* ((old-fn (symbol-function #'corfu--format-candidates))
 	     (new-fn (apply-partially #'corfu-quick--format-candidates old-fn))
 	     ((symbol-function #'corfu--format-candidates) new-fn))
+    (corfu--update)
     1
     ;; (alist-get (read-key) corfu-quick--list)
     ))
