fd12a85d5111a8ca52bfe064d7c2e8ca682688f4
Luis Henriquez-Perez <luis@luishp.xyz>
2022-01-12 10:26:11 -0500

    Update corfu-quick

--- corfu-quick.el
+++ corfu-quick.el
@@ -107,7 +107,7 @@
     (pcase-dolist (`(,candidate ,prefix ,suffix) candidates)
       ;; Update the prefix.
       (setq quick-letters (corfu-quick--letters))
-      (setq prefix (concat "" quick-letters))
+      ;; (setq prefix (concat "" quick-letters))
       (push (list candidate prefix suffix) updated-candidates))
     (cl-callf reverse updated-candidates)
     (funcall orig updated-candidates)))
