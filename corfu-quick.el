6fd51576699c5197d6c79b7b228644d61267c26e
Luis Henriquez-Perez <luis@luishp.xyz>
2022-01-12 09:16:53 -0500

    Update corfu-quick

--- corfu-quick.el
+++ corfu-quick.el
@@ -113,7 +113,8 @@
 (defun corfu-quick-jump ()
   "Jump to candidate using quick-keys."
   (interactive)
-  (cl-letf* (((symbol-function #'corfu--format-candidate)
+  (cl-letf* (()
+	     ((symbol-function #'corfu--format-candidate)
 	      (apply-partially #'corfu-quick--format-candidate
 			       (symbol-function #'corfu--format-candidate))))
     (alist-get (read-key) corfu-quick--list)
